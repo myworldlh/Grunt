dd7af59b272cd0f60402ac8173955260ba80eaa1
myworldlh <623518741@qq.com>
2018-09-05 17:13:29 +0800

    yeoman,bower study code

---
+++ yo-in-action/angular-in-action/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name learnangularApp
+ * @description
+ * # learnangularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('learnangularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
